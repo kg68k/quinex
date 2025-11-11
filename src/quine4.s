93af38f66a4529921d767d5001482d872779c7dc
kg68k <78926718+kg68k@users.noreply.github.com>
2025-11-12 02:01:39 +0900

    fix (quine4): typo in title

--- src/quine4.s
+++ src/quine4.s
@@ -1,4 +1,4 @@
-.title quine1
+.title quine4
 .xref __main,_printf
 _main::
   link a6,#0
@@ -16,5 +16,5 @@ _main::
   unlk a6
   moveq #0,d0
   rts
-S: .dc.b '.title quine1%c.xref __main,_printf%c_main::%c  link a6,#0%c  moveq #10,d0%c  move.l d0,-(sp)%c  move.l d0,-(sp)%c  pea (39).w%c  pea (S,pc)%c  pea (39).w%c  moveq #18-1,d1%c  @@: move.l d0,-(sp)%c  dbra d1,@b%c  pea (S,pc)%c  jbsr (_printf)%c  unlk a6%c  moveq #0,d0%c  rts%cS: .dc.b %c%s%c,0%c.end%c',0
+S: .dc.b '.title quine4%c.xref __main,_printf%c_main::%c  link a6,#0%c  moveq #10,d0%c  move.l d0,-(sp)%c  move.l d0,-(sp)%c  pea (39).w%c  pea (S,pc)%c  pea (39).w%c  moveq #18-1,d1%c  @@: move.l d0,-(sp)%c  dbra d1,@b%c  pea (S,pc)%c  jbsr (_printf)%c  unlk a6%c  moveq #0,d0%c  rts%cS: .dc.b %c%s%c,0%c.end%c',0
 .end
